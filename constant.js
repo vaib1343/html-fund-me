06e9135856ca830880a10b6a1e2a52e2e5de5096
Vaibhav Varshney <vaibhavvarshney@Vaibhavs-MacBook-Air.local>
2023-03-01 23:30:27 +0530

    connect wallet and contract

---
+++ constant.js
@@ -0,0 +1,132 @@
+export const contractAddress = "0xe3462110C52730F2cF5Cb099E3Cc0289EE52A017";
+
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "priceFeedAddress",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "Fundme__NotOwner",
+      "type": "error"
+    },
+    {
+      "stateMutability": "payable",
+      "type": "fallback"
+    },
+    {
+      "inputs": [],
+      "name": "MINIMUN_USD",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "addressToAmountFunded",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "fund",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "funders",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getVersion",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "i_owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "priceFeed",
+      "outputs": [
+        {
+          "internalType": "contract AggregatorV3Interface",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdraw",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "stateMutability": "payable",
+      "type": "receive"
+    }
+  ]
--- index.js
+++ index.js
@@ -1,21 +1,84 @@
+import { ethers } from "./ethers-5.6.esm.min.js";
+import { contractAddress, abi } from "./constant.js";
+
+const fundButton = document.getElementById("fund_button");
+const connectButton = document.getElementById("connect_button");
+const balanceButton = document.getElementById("balance_button");
+const withdrawButton = document.getElementById("withdraw_button");
+
+fundButton.onclick = fund;
+connectButton.onclick = connect;
+balanceButton.onclick = getBalance;
+withdrawButton.onclick = withdraw;
+
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
         console.log("metamack detected");
         await window.ethereum.request({
             method: "eth_requestAccounts",
         });
-        document.getElementById("connect_button").innerHTML = "connected";
+        connectButton.innerHTML = "connected";
         console.log("wallet connected");
     } else {
         console.log("please install metamask");
-        document.getElementById("connect_button").innerHTML =
-            "Please install metamask";
+        connectButton.innerHTML = "Please install metamask";
+    }
+}
+
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const balance = await provider.getBalance(contractAddress);
+        console.log(`contract balance: ${balance}`);
+    } else {
+        console.log("please install metamask");
     }
 }
 
 async function fund(ethAmount) {
+    const etherAmount = document.getElementById("ethAmount");
+
     console.log(`Funding with ${ethAmount}`);
     // provider / connection to the blockchain
     // signer / wallet/ someone with gas
     // contract that we are interaction with (ABI and address)
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const fundMeContract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+        const transactionResponse = await fundMeContract.fund({
+            value: ethers.utils.parseEther(etherAmount),
+        });
+        await listenForTransaction(transactionResponse, provider);
+        console.log("done");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function listenForTransaction(transactionResponse, provider) {
+    console.log(`transaction: ${transactionResponse.hash}`);
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(`confirmation ${transactionReceipt.confirmations}`);
+            resolve();
+        });
+    });
+}
+
+async function withdraw() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const fundMeContract = new ethers.Contract(
+            contractAddress,
+            abi,
+            signer
+        );
+        const transactionResponse = await fundMeContract.withdraw();
+        await listenForTransaction(transactionResponse, provider);
+    } else {
+        console.log("please install wallter");
+    }
 }
